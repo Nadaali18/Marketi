fcdc2f0d93ae8534fe2311653f87c19d930b8932
nadaali <nadaeloksh55@gmail.com>
2025-03-04 17:51:50 +0200

    finish signup screen

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:marketi/core/helpers/colors.dart';
+import 'package:marketi/core/widgets/custom_text.dart';
 
 class CustomTextFormField extends StatefulWidget {
-  const CustomTextFormField({super.key,required this.hintText,this.iconPre,this.iconSuf,this.iconSuf1});
+  const CustomTextFormField({super.key,this.text,required this.hintText,this.iconPre,this.iconSuf,this.iconSuf1});
 final IconData? iconPre;
 final IconData? iconSuf;
 final IconData? iconSuf1;
 final String hintText;
+final String? text;
 
   @override
   State<CustomTextFormField> createState() => _CustomTextFormFieldState();
@@ -18,35 +20,41 @@ bool obscure = false;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 15),
-      child: SizedBox(
-        width: double.infinity,
-        height: 50,
-        child: TextFormField(
-          cursorColor: secondColor,
-          obscureText: obscure,
-          decoration: InputDecoration(
-            prefixIcon: Icon(widget.iconPre,color: Colors.black,size: 15,),
-            suffixIcon: IconButton(
-              icon: Icon(obscure ? widget.iconSuf : widget.iconSuf1,color: Colors.black,size: 15,),
-              onPressed: (){
-              setState(() {
-                obscure = !obscure;
-              });
-            }, ),
-            hintText: widget.hintText,
-            hintStyle: TextStyle(color: hintTextColor),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-              borderSide: BorderSide(color: dotColor.withValues(alpha: 178),width: 2),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-              borderSide: BorderSide(color: dotColor.withValues(alpha: 178),width: 2),
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomText(widget.text ?? '',fontWeight: FontWeight.bold,),
+          SizedBox(
+            width: double.infinity,
+            height: 50,
+            child: TextFormField(
+              cursorColor: secondColor,
+              obscureText: obscure,
+              decoration: InputDecoration(
+                prefixIcon: Icon(widget.iconPre,color: Colors.black,size: 15,),
+                suffixIcon: IconButton(
+                  icon: Icon(obscure ? widget.iconSuf : widget.iconSuf1,color: Colors.black,size: 15,),
+                  onPressed: (){
+                  setState(() {
+                    obscure = !obscure;
+                  });
+                }, ),
+                hintText: widget.hintText,
+                hintStyle: TextStyle(color: hintTextColor),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide(color: dotColor.withValues(alpha: 178),width: 2),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide(color: dotColor.withValues(alpha: 178),width: 2),
+                ),
+              ),
             ),
           ),
-        ),
+        ],
       ),
-      );
+    );
   }
 }
--- lib/features/signUp/widget/signup_body.dart
+++ lib/features/signUp/widget/signup_body.dart
@@ -5,27 +5,42 @@ import 'package:marketi/core/widgets/custom_column.dart';
 import 'package:marketi/core/widgets/custom_elevated_button.dart';
 import 'package:marketi/core/widgets/custom_icon_button.dart';
 import 'package:marketi/core/widgets/custom_roww.dart';
+import 'package:marketi/core/widgets/custom_text_form_field.dart';
 
 class SignupBody extends StatelessWidget {
   const SignupBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-            padding: const EdgeInsets.all(15),
-            child: CustomIconButton(icon: Icon(Icons.arrow_back_ios_new),value: 15,),
-        ),
-        Center(
-            child: Image.asset('assets/images/Logo_Splash_Screen.png',height: 150,width: 150,),
-        ),
-        CustomElevatedButton(text: 'Sign Up', function: (){}),
-        CustomColumn(),
-        SizedBox(height: 5,),
-        CustomRoww(text: 'Already, have an account ', text1:'login?',function: () => context.go(Routes.login),),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+              padding: const EdgeInsets.all(15),
+              child: CustomIconButton(icon: Icon(Icons.arrow_back_ios_new),value: 30,),
+          ),
+          Center(
+              child: Image.asset('assets/images/Logo_Splash_Screen.png',height: 200,width: 200,),
+          ),
+          CustomTextFormField(text: 'Your Name',iconPre: Icons.person,hintText: 'Full Name',),
+          SizedBox(height: 3,),
+          CustomTextFormField(text: 'Username',iconPre: Icons.person_2,hintText: 'Username'),
+          SizedBox(height: 3,),
+          CustomTextFormField(text: 'Phone Number',iconPre: Icons.phone_android_outlined,hintText: '+20 1501142409 '),
+          SizedBox(height: 3,),
+          CustomTextFormField(text: 'Email',iconPre: Icons.email_outlined,hintText: 'You@gmail.com'),
+          SizedBox(height: 3,),
+          CustomTextFormField(text: 'Password',iconPre: Icons.lock_outline_rounded,hintText: 'Your Password',iconSuf: Icons.visibility,iconSuf1: Icons.visibility_off,),
+          SizedBox(height: 3,),
+          CustomTextFormField(text: 'Confirm Password',iconPre: Icons.lock_outline_rounded,hintText: 'Confirm Your Password',iconSuf: Icons.visibility,iconSuf1: Icons.visibility_off,),
+          CustomElevatedButton(text: 'Sign Up', function: (){}),
+          CustomColumn(),
+          SizedBox(height: 5,),
+          CustomRoww(text: 'Already, have an account ', text1:'login?',function: () => context.go(Routes.login),),
+          SizedBox(height: 10,),
+        ],
+      ),
     );
   }
 }
